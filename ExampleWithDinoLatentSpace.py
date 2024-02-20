5433bb4390ab10dc04acb84d351e88773801649a
computer <computador@computer>
2024-02-20 11:03:34 -0300

    - Starting script to use Dino with latent space

---
+++ ExampleWithDinoLatentSpace.py
@@ -0,0 +1,77 @@
+from torchvision import transforms
+import torch
+import torch.nn as nn
+import numpy as np
+
+
+class Block(nn.Module):
+    def __init__(self, n_in, n_out):
+        super().__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(n_in, n_out, 3, 1, 1),
+            nn.ReLU(),
+            nn.Conv2d(n_in, n_out, 3, 1, 1),
+            nn.ReLU(),
+            nn.Conv2d(n_in, n_out, 3, 1, 1))
+        self.fuse = nn.ReLU()
+    def forward(self, x):
+        return self.fuse(self.conv(x) + x)
+
+class DinoLatentHeader(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.train_layer = nn.Sequential(
+            nn.Conv2d(4, 384, 3, 1, 1),
+            nn.ReLU(),
+            Block(384, 384),
+            Block(384, 384),
+            Block(384, 384),
+            nn.Conv2d(384, 384, 2, 2),
+            Block(384, 384),
+            Block(384, 384),
+            Block(384, 384),
+            nn.Conv2d(384, 384, 3, 1, 1, bias=False),
+        )
+
+
+    def forward(self, x):
+        return self.train_layer(x)
+
+def DinoWithLatentHeader(header_path, device="cpu"):
+  model = torch.hub.load('facebookresearch/dinov2', 'dinov2_vits14_reg').to(device)
+  head = DinoLatentHeader()
+  head.load_state_dict(torch.load(header_path, map_location=device))
+  model.patch_embed.proj = head
+  model.patch_size = 2
+  return model
+  
+  
+model = DinoWithLatentHeader("/path/to/model/file/dino_header_rl.pkl")
+
+#Will transform the input before feeding it to Dino
+transform = transforms.Compose([
+            
+        ])
+
+#Create a custom normalized function class
+#loss_func can also be nn.L1Loss(), nn.MSELoss(), or any similar, if you don't want to normalize the tensors.
+loss_func = NormalizedTensorLoss("l2")
+
+
+#Create the configuration for the main class
+config = GlobalPercConfig(start_weight=1.,
+                          end_weight=2.,
+                          modules_to_hook=[nn.Linear, nn.Conv2d, nn.ReLU, nn.GELU],
+                          transform_normalization=transform,
+                          loss_func=loss_func,
+                          print_data = True
+                          )
+
+#Generate the loss class
+loss = GlobalPercLoss(model, config)
+#Latent Tensors have 4 channels so input is [B, 4, H, W]
+tensor_1 = torch.rand(1, 4, 64, 64)
+tensor_2 = torch.rand(1, 4, 64, 64)
+loss(tensor_1, tensor_2)
+
